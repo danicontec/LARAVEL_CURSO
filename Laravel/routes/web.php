51defba0aa745fb50f4047623b6497d7556406c0
danicontec <daniel.mrt.bkt@gmail.com>
2023-06-15 15:53:06 +0200

    Añadidos ejemplos RAW para trabajar con las tablas creadas con los archivos de migrations

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -30,3 +30,54 @@ Route::get('/contacto', 'App\Http\Controllers\WebPagesController@contacto');
 // La instruccion de abajo crea los links automaticamente, siempre y cuando hayamos creado el Controller con la opcion --resource
 
 // Route::resource("post","App\Http\Controllers\WebPagesController");
+
+//La funcio get tambien puede recibir como parametro una funcion anónima para trabajar con nla base de datos
+
+Route::get('/bbdd', function(){
+
+    //Para hacer consultas, este metodo tambien devuelve array dimensional
+    $data = DB::select("SELECT * FROM ARTICULOSNUEVOS");
+
+    foreach($data as $values){
+        foreach($values as $strings){
+
+            echo " ". $strings   ;
+        }
+        echo "<br>";
+    }
+});
+
+Route::get('/update', function(){
+    $update = DB::update("UPDATE ARTICULOSNUEVOS SET NOMBRE='PINGU' WHERE ID=1");
+    
+    if($update){
+        echo "Registro actualizado";
+    }
+
+    else{
+        echo "Fallo al actualizar";
+    }
+});
+
+Route::get('/delete', function(){
+    $delete = DB::delete("DELETE FROM ARTICULOSNUEVOS WHERE ID=2");
+
+    if($delete){
+        echo "Registro borrado";
+    }
+
+    else {
+        echo "No se ha podido borrar el registro";
+    }
+} );
+
+Route::get('/insert', function(){
+    $insert = DB::insert("INSERT INTO ARTICULOSNUEVOS (NOMBRE, DESCRIPCION, IMAGEN, PAIS, PRECIO, OBSERVACIONES) VALUES ('JUGUETE', 'JUGUETE DE NIÑOS', 'NO', 'ESPAÑA', 12.30, 'PRODUCTO POR DESCATALOGAR')");
+
+    if($insert){
+        echo "Datos insertados correctamente";
+    } else {
+
+        echo "Fallo al insertar registros";
+    }
+});
