722be31397654fb250ae2a9cf0f5705328801986
danicontec <daniel.mrt.bkt@gmail.com>
2023-07-01 17:41:42 +0200

    Inicio y prueba de CRUD con Laravel (ejercicio)

---
+++ Laravel-CRUD/database/migrations/2023_07_01_143421_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre');
+            $table->string('Seccion');
+            $table->double('Precio');
+            $table->string('Fecha');
+            $table->string('Origen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
